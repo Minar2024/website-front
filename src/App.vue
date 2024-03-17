2cb237be707a0b044b774edbe43867c2f61b0510
TianWen <1907499849@qq.com>
2024-03-17 11:30:39 +0800

    template

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="item in userList" :key="item.id">
+      {{ item.name +'---'+ item.age}}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+const userList = ref([])
+onMounted(() => {
+  axios.get("http://localhost:3000/").then((res) => {
+    userList.value = res.data
+  })
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
